c76e13d38aa0c3fda097fe76d73c19107351a59c
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-02-28 13:57:52 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 
 # Create your views here.
+
+
 def view_bag(request):
-    """
-    A view that renders the bag contents page
-    """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
-    """
-    Add a quantity of the specified product to the shopping bag
-    """
+    """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -35,15 +36,14 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
-    """
-    Adjust the quantity of the specified product to the specified amount
-    """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -69,9 +69,8 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """
-    Remove the item from the shopping bag
-    """
+    """Remove the item from the shopping bag"""
+
     try:
         size = None
         if 'product_size' in request.POST:
@@ -82,7 +81,6 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-
         else:
             bag.pop(item_id)
 
@@ -91,4 +89,3 @@ def remove_from_bag(request, item_id):
 
     except Exception as e:
         return HttpResponse(status=500)
-        
